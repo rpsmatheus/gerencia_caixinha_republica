876a82cdb8d8fc060783feabb204cdf85fd0dd2e
Bianca <barretoleme05@gmail.com>
2026-06-07 19:42:48 -0300

    feat: stopped ignoring ExpenseRepository

--- backend/src/app/appContext.ts
+++ backend/src/app/appContext.ts
@@ -1,9 +1,9 @@
 
 import { ResidentRepository } from '../repositories/ResidentRepository.js';
-// import { ExpenseRepository } from '../repositories/ExpenseRepository.js';
+import { ExpenseRepository } from '../repositories/ExpenseRepository.js';
 // import { CategoryRepository } from '../repositories/CategoryRepository.js';
 
 // Instanciação global para o projeto usar
 export const residentRepo = new ResidentRepository();
-// export const expenseRepo = new ExpenseRepository();
+export const expenseRepo = new ExpenseRepository();
 // export const categoryRepo = new CategoryRepository();
