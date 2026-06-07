c061337540cef6da4b61d1eec9538b9dacff55a6
Matheus Rodrigues <matheusarp70@gmail.com>
2026-06-07 19:09:50 -0300

    feat: implementação do módulo de orçamentos, permitindo definição de limites de gastos

--- backend/src/app/routes.ts
+++ backend/src/app/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { residentRoutes } from '../modules/residents/residents.routes.js';
-import { categoryRoutes } from '../modules/categories/categories.routes.js'; // Adicione esse import
+import { categoryRoutes } from '../modules/categories/categories.routes.js';
+import { budgetRoutes } from '../modules/budgets/budgets.routes.js'; 
 
 const router = Router();
 
 router.use('/residents', residentRoutes);
-router.use('/categories', categoryRoutes); // Adicione essa linha
+router.use('/categories', categoryRoutes);
+router.use('/budgets', budgetRoutes); 
 
 export default router;
